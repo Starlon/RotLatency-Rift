6b6da7617fd5edaac2db2e2c839d88e04a0ae32a
Scott Sibley <sisibley@gmail.com>
2011-09-15 08:10:13 -0500

    Bug fix.

--- RotLatency.lua
+++ RotLatency.lua
@@ -8,6 +8,7 @@ local text = UI.CreateFrame("Text", "Text", ctx)
 text:SetPoint("CENTER", UIParent, "CENTER")
 text:SetFontSize(15)
 text:SetVisible(false)
+local ability = "Path of the Wind"
 
 table.insert(Event.Ability.Cooldown.Begin, {function(cooldowns)
 	local time = GetTime()
@@ -22,7 +23,9 @@ table.insert(Event.Ability.Cooldown.End, {function(cooldowns)
 	local time = GetTime()
 	for id, v in pairs(cooldowns) do
 		local entry = data[id] and data[id][#data[id]]
-		if entry and data[id].cooldown then entry.finish = GetTime() end
+		if entry and data[id].cooldown then 
+			entry.finish = GetTime() 
+		end
 	end
 end, "RotLatency", "refresh"})
 
@@ -35,12 +38,21 @@ local function update()
 		for id, v in pairs(data) do
 			local entry1 = v[#v]
 			local entry2 = v[#v - 1]
-			if entry2 and entry2.finish and v.name == "Path of the Wind" then
-				if entry2.finish - entry2.start + .1 > v.cooldown and entry1.remaining > v.cooldown - .1 then
-					text:SetText(v.name .. ": " .. format("%.2f", entry1.start - entry2.finish))
+			if entry2 and v.name == ability then
+				entry2.finish = entry2.finish or entry2.start
+				local tm1 = tonumber(format("%.2f", entry2.finish - entry2.start + .02))
+				local tm2 = tonumber(format("%.2f", entry1.remaining))
+				local tm3 = tonumber(format("%.2f", v.cooldown - .02))
+				local tm1 = tonumber(format("%.2f", entry2.finish - entry2.start))
+				local tm2 = tonumber(format("%.2f", entry1.remaining))
+				local tm3 = tonumber(format("%.2f", v.cooldown))
+				if tm1 >= tm3 and tm2 >= tm3 then
+					count = (count or 0) + 1
+					text:SetText("(" .. count .. ") " ..  v.name .. ": " .. format("%.2f", entry1.start - entry2.finish))
 					text:ResizeToText()
 					text:SetVisible(true)
 				else
+					text:SetText("wtf")
 					text:SetVisible(false)
 				end
 			end
