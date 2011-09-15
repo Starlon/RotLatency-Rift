7bbb22cefa12947b8ad3b3221c68294a2a0933c5
Scott Sibley <sisibley@gmail.com>
2011-09-15 06:48:59 -0500

    Bug fix.

--- RotLatency.lua
+++ RotLatency.lua
@@ -8,12 +8,13 @@ local text = UI.CreateFrame("Text", "Text", ctx)
 text:SetPoint("CENTER", UIParent, "CENTER")
 text:SetFontSize(15)
 text:SetVisible(false)
+
 table.insert(Event.Ability.Cooldown.Begin, {function(cooldowns)
 	local time = GetTime()
 	for id, v in pairs(cooldowns) do
 		local ability = Inspect.Ability.Detail(id)
 		data[id] = data[id] or {cooldown=ability.cooldown, name=ability.name}	
-		tinsert(data[id], {start=time})
+		tinsert(data[id], {start=time, remaining=ability.currentCooldownRemaining})
 	end
 end, "RotLatency", "refresh"})
 
@@ -35,7 +36,7 @@ local function update()
 			local entry1 = v[#v]
 			local entry2 = v[#v - 1]
 			if entry2 and entry2.finish and v.name == "Path of the Wind" then
-				if entry2.finish - entry2.start + .1 > v.cooldown then
+				if entry2.finish - entry2.start + .1 > v.cooldown and entry1.remaining > v.cooldown - .1 then
 					text:SetText(v.name .. ": " .. format("%.2f", entry1.start - entry2.finish))
 					text:ResizeToText()
 					text:SetVisible(true)
